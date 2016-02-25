c8fbeab9b7742d67501577ee2d5f87422d4b078e
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-02-25 09:55:31 +0100

    adding a root TemplateView

--- django-threejs/threejs/urls.py
+++ django-threejs/threejs/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', TemplateView.as_view(template_name="index.html")),
 ]

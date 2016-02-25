2aee6465bdd1049365fef1003815de7c4b35e4b7
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-02-25 09:51:12 +0100

    providing valid configurations for Wheelie

--- django-threejs/wheelie/gulpfile.js
+++ django-threejs/wheelie/gulpfile.js
@@ -1,22 +1,19 @@
-require('gulp');
-
-// importing Wheelie instance and the base Wheelie recipe
+var gulp = require('gulp');
 var wheelie = require('wheelie');
 var recipe = require('wheelie-recipe');
 
-// adding the base recipe to Wheelie
 wheelie.add(recipe);
 wheelie.setDefault('watch');
 
 // build customizations
 var vendors = [
-    // put here your vendors installed via bower, npm, or whatever
-    // with a base PATH of `client/vendors/` folder
+    // put here your vendors installed via bower (or whatever), available in `vendors/` folder
+    'three.js/build/three.min.js'
 ];
 
 var scripts = [
-    // add here your JavaScript files
-    // with a base PATH of `client/js/` folder
+    // add here your JS files available in `client/js/` folder
+    'main.js'
 ];
 
 wheelie.update('uglify', {
@@ -24,8 +21,6 @@ wheelie.update('uglify', {
   vendors: vendors
 });
 
-// the production and the live building, output processed files
-// in the `wheelie/static/` folder
 wheelie.setBuild('static/');
 wheelie.setDist('static/');
 wheelie.build();

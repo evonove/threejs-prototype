894ca9d56a436cb148fa74efa26cf5ce93198d9d
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-02-25 09:43:30 +0100

    removing failing test

--- django-threejs/tests/simple/test_simple.py
+++ django-threejs/tests/simple/test_simple.py
@@ -4,10 +4,3 @@ def test_simple_empty_test():
     test your CI integration
     """
     assert 1 == 1
-
-def test_simple_empty_test_failure():
-    """
-    This test is used as a placeholder so you can immediately
-    test your CI integration
-    """
-    assert 0 == 1
